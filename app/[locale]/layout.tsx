9ebfa21ea080cf871c8f1d5b295748290c15e9c3
Mohammad Saquib <saquibe143@gmail.com>
2026-05-12 15:41:31 +0530

    update metadata for Events Craft and adjust eslint-config-next version

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -14,8 +14,8 @@ import DirectionProvider from "@/providers/direction-provider";
 import AuthProvider from "@/providers/auth.provider";
 
 export const metadata: Metadata = {
-  title: "Dashcode admin Template",
-  description: "created by codeshaper",
+  title: "Events Craft",
+  description: "created by SaasCraft Studio",
 };
 
 export default async function RootLayout({
